be85768fa3e3348d0fb3b809df1ef517931b7c5e
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-16 01:34:00 +0530

    navbar completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,27 +1,54 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+import { GithubIcon, LinkedInIcon, TwitterIcon } from "./Icons";
+import {motion} from "framer-motion";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/articles">Articles</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
-      <h2>Logo</h2>
-      <nav>
-        <Link href="/" target={"_blank"}>
-          L
-        </Link>
-        <Link href="/" target={"_blank"}>
-          G
-        </Link>
-        <Link href="/" target={"_blank"}>
-          I
-        </Link>
+
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a href="https://www.linkedin.com/in/madura-prasad/" target={"_blank"} whileHover={{y:-2}} className="w-6 mx-3" whileTap={{scale
+        :0.9}}>
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="https://github.com/Madura-Prasad" target={"_blank"} whileHover={{y:-2}} className="w-6 mx-3" whileTap={{scale
+        :0.9}} >
+          <GithubIcon />
+        </motion.a>
+        <motion.a href="https://twitter.com/MaduraPrasadLk" target={"_blank"} whileHover={{y:-2}} className="w-6 mx-3" whileTap={{scale
+        :0.9}} >
+          <TwitterIcon />
+        </motion.a>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
