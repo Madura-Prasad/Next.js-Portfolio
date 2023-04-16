4e69c23bdaacf6cb0ff49c67d75e79e82a364ec3
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-16 20:08:53 +0530

    created about page

---
+++ src/pages/about.js
@@ -0,0 +1,113 @@
+import AnimatedText from "@/component/AnimatedText";
+import Layout from "@/component/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/1111.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Madura Prasad | About page</title>
+        <meta name="description" content="Madura Prasad" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Exploring my Background, Skills and Passion."
+            className="mb-16"
+          />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="text-justify font-semibold">
+                Hello, I am Madura Prasad, I'm a highly motivated software
+                engineering graduate with a Bachelor of Science degree. I have
+                experience with various programming languages, including Java,
+                PHP, Springboot, HTML, MYSQL, OOP, Javascript, Android App
+                Development, React JS, and React Native. My career objective is
+                to become a successful software engineer by using my knowledge
+                and skills to develop innovative software solutions that address
+                the requirements of businesses and end-users. With their
+                education and experience, Madura Prasad is well-positioned to
+                achieve their professional goals and excel in the software
+                engineering industry.
+              </p>
+            </div>
+
+            <div className="col-span-2 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Madura Prasad"
+                className="w-80 h-90 rounded-2xl"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  {" "}
+                  <AnimatedNumbers value={34} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Satisfied Clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  {" "}
+                  <AnimatedNumbers value={15} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Projects Completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  {" "}
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
