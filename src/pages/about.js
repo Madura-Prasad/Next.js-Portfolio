7020723eea97280ed298251484f90117ab02d1db
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-17 00:12:15 +0530

    update about page projects

--- src/pages/about.js
+++ src/pages/about.js
@@ -86,7 +86,7 @@ const about = () => {
               <div className="flex flex-col items-end justify-center">
                 <span className="inline-block text-7xl font-bold">
                   {" "}
-                  <AnimatedNumbers value={15} />+
+                  <AnimatedNumbers value={18} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75">
                   Projects Completed
