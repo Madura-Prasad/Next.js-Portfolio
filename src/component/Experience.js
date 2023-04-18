8e17382510a54c1840ddbb56cfdff178c82fea40
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-19 01:55:39 +0530

    update education and experience animation

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -39,7 +39,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
         style={{scaleY:scrollYProgress}}
-        className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top" />
+        className="absolute left-9 top-1 w-[4px] h-full bg-dark origin-top" />
         <ul className="w-full flex flex-col items-start justify-between ml-5">
           <Details
             position="Software Engineer Intern"
