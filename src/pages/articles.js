f401b8c884bba3236e4b7682d9cf4bdd9efe116c
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-26 15:07:48 +0530

    add projects,articles

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,7 +10,7 @@ const Article = ({ title, date, link }) => {
     <motion.li
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
-      viewport={{once:true}}
+      viewport={{ once: true }}
       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 cursor-pointer
     "
     >
@@ -38,11 +38,191 @@ const articles = () => {
             className="mb-16 cursor-pointer"
           />
           <ul>
-            <Article title="Test" date="Test" link="/" />
+            <Article
+              title="Mastering Spring Boot: A Guide to Building Robust Java Web Applications"
+              date="2023 Apr 16"
+              link="https://medium.com/@maduraprasad/mastering-spring-boot-a-guide-to-building-robust-java-web-applications-dc5f4cf8b19c"
+            />
+            <Article
+              title="Getting started with Java Spring Boot data testing using Postman"
+              date="2023 Apr 4"
+              link="https://medium.com/@maduraprasad/getting-started-with-java-spring-boot-data-testing-using-postman-fb9cdfac5266"
+            />
 
-            <Article title="Test" date="Test" link="/" />
-            <Article title="Test" date="Test" link="/" />
-            <Article title="Test" date="Test" link="/" />
+            <Article
+              title="Building a RESTful API with Java Spring Boot and CRUD Operations"
+              date="2023 Mar 30"
+              link="https://medium.com/@maduraprasad/building-a-restful-api-with-java-spring-boot-and-crud-operations-5c17d2ef8e84"
+            />
+
+            <Article
+              title="The Basics of Spring Component Scanning and Annotations"
+              date="2023 Mar 29"
+              link="https://medium.com/@maduraprasad/the-basics-of-spring-component-scanning-and-annotations-e531d48c0331"
+            />
+
+            <Article
+              title="The Ultimate Guide to Becoming a Java Spring Boot Developer"
+              date="2023 Mar 23"
+              link="https://medium.com/@maduraprasad/the-ultimate-guide-to-becoming-a-java-spring-boot-developer-dbd4415ee993"
+            />
+
+            <Article
+              title="A Beginner’s Guide to Starting a Career in React Native Development"
+              date="2023 Mar 12"
+              link="https://medium.com/@maduraprasad/a-beginners-guide-to-starting-a-career-in-react-native-development-b8db3d7774c"
+            />
+
+            <Article
+              title="9 Outstanding JavaScript Projects for Your CV"
+              date="2023 Feb 19"
+              link="https://medium.com/@maduraprasad/nine-outstanding-javascript-projects-for-your-cv-dc6c34c18992"
+            />
+
+            <Article
+              title="SE Student Final Year Project Ideas"
+              date="2023 Feb 02"
+              link="https://medium.com/@maduraprasad/se-student-final-year-pr-ff01e6d3f2de"
+            />
+
+            <Article
+              title="Learn Android App Development for Beginners: Create Your First App"
+              date="2023 Jan 29"
+              link="https://medium.com/@maduraprasad/a-simple-calculator-app-this-project-can-help-beginner-app-developers-understand-the-basics-of-291d48800012"
+            />
+
+            <Article
+              title="A Detailed Comparison of React Native and Flutter for Mobile App Development"
+              date="2023 Jan 24"
+              link="https://medium.com/@maduraprasad/react-native-and-flutter-are-two-popular-frameworks-for-building-mobile-apps-d6de9aa5285b"
+            />
+
+            <Article
+              title="Effective Error Handling in React: A Developer’s Guide"
+              date="2023 Jan 24"
+              link="https://medium.com/@maduraprasad/error-handling-is-a-necessary-component-of-any-software-development-process-97598f9fe65e"
+            />
+
+            <Article
+              title="5 Proven Strategies for Optimizing Your React App’s Performance"
+              date="2023 Jan 24"
+              link="https://medium.com/@maduraprasad/use-the-react-developer-tools-the-react-developer-tools-is-a-browser-extension-that-allows-you-to-24f242a50f14"
+            />
+
+            <Article
+              title="Why We Need JavaScript Frameworks: A Look at the Top 5"
+              date="2023 Jan 23"
+              link="https://medium.com/@maduraprasad/why-we-need-javascript-frameworks-a-look-at-the-top-5-281edf237542"
+            />
+
+            <Article
+              title="2023 Web Development Roadmap: Key Technologies and Trends to Watch"
+              date="2023 Jan 21"
+              link="https://medium.com/@maduraprasad/2023-web-development-roadmap-key-technologies-and-trends-to-watch-19f469da2f49"
+            />
+
+            <Article
+              title="Is React Native Worth Learning for Mobile App Development?"
+              date="2023 Jan 20"
+              link="https://medium.com/@maduraprasad/is-react-native-worth-learning-for-mobile-app-development-b62d33154eae"
+            />
+
+            <Article
+              title="In-Demand and High-Paying Programming Jobs for the Future"
+              date="2023 jan 20"
+              link="https://medium.com/@maduraprasad/depending-on-criteria-like-location-industry-and-experience-expertise-there-are-a-number-of-33aa3bd238d8"
+            />
+
+            <Article
+              title="The Impact of AI on Software Engineers: Opportunities and Risks"
+              date="2023 Jan 20"
+              link="https://medium.com/@maduraprasad/the-ai-revolution-will-definitely-change-a-lot-about-what-software-engineers-do-and-how-they-do-it-ac559fe16ed0"
+            />
+
+            <Article
+              title="Navigating the Road to Success: How to Choose a Career Path for Students"
+              date="2023 jan 19"
+              link="https://medium.com/@maduraprasad/choosing-a-career-path-for-a-student-can-be-a-daunting-task-but-with-the-right-guidance-and-f89cf0306e83"
+            />
+
+            <Article
+              title="Mastering the Art of Writing Clean Code"
+              date="2023 Jan 19"
+              link="https://medium.com/@maduraprasad/mastering-the-art-of-writing-clean-code-d3d8a573c579"
+            />
+
+            <Article
+              title="Top Websites Computer Science Students Must Visit"
+              date="2023 Jan 19"
+              link="https://medium.com/@maduraprasad/top-websites-computer-science-students-must-visit-fdcf45e5538f"
+            />
+
+            <Article
+              title="Preparing for your Interview: Tips and Tricks for Applying to Software Engineer Jobs in a Foreign Country"
+              date="2023 Jan 18"
+              link="https://medium.com/@maduraprasad/preparing-for-your-interview-tips-and-tricks-for-applying-to-software-engineer-jobs-in-a-foreign-38bc91f0e41c"
+            />
+
+            <Article
+              title="The Home-Based Software Developer’s Guide to Earning Passive Income"
+              date="2023 Jan 18"
+              link="https://medium.com/@maduraprasad/the-home-based-software-developers-guide-to-earning-passive-income-6c658136d9fb"
+            />
+            <Article
+              title="From Resume to Internship: A Guide to Getting a Software Engineering Internship"
+              date="2023 Jan 18"
+              link="https://medium.com/@maduraprasad/from-resume-to-internship-a-guide-to-getting-a-software-engineering-internship-4c275874e9b"
+            />
+
+            <Article
+              title="From Beginner to Full Stack Java Developer: A Career Roadmap"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/a-full-stack-java-developer-is-a-highly-experienced-software-engineer-who-can-work-on-both-the-9d4036d4134b"
+            />
+
+            <Article
+              title="From Beginner to Full Stack Java Developer: A Career Roadmap"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/a-full-stack-java-developer-is-a-highly-experienced-software-engineer-who-can-work-on-both-the-9d4036d4134b"
+            />
+            <Article
+              title="Object-Oriented Programming vs Functional Programming: Which is Better for Your Project?"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/object-oriented-programming-vs-functional-programming-which-is-better-for-your-project-ed4c27dc78f4"
+            />
+
+            <Article
+              title="Java vs Kotlin: A Comparison of Two Popular Programming Languages for Android Development"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/java-kotlin-pros-and-cons-3e165302eed9"
+            />
+            <Article
+              title="Creating a Strong CV as an IT Fresher: Tips and Tricks"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/how-to-create-a-strong-cv-as-an-it-fresher-5fb1ddb2bc46"
+            />
+            <Article
+              title="A Beginner’s Guide to Android App Development with Android Studio"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/create-first-android-app-using-android-studio-390bbeea559a"
+            />
+
+            <Article
+              title="Mastering the Art of Online Programming: Strategies for Success"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/how-can-a-programmer-be-successful-on-online-platforms-c31a99cf61ac"
+            />
+
+            <Article
+              title="From Zero to Hero: A Roadmap for Becoming an Android Developer"
+              date="2023 Jan 17"
+              link="https://medium.com/@maduraprasad/how-to-become-an-android-developer-a-step-by-step-guide-e1173e6840e3"
+            />
+            <Article
+              title="7 Books You Should Read to Improve Your Software Development Skills"
+              date="2023 Jan 16"
+              link="https://medium.com/@maduraprasad/7-books-you-should-read-to-improve-your-software-development-skills-2e35b384601d"
+            />
           </ul>
         </Layout>
       </main>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,7 +6,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import {motion} from "framer-motion";
-import Project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import constructionApp from "../../public/images/projects/constructionApp.jpg";
+import Warehouse from "../../public/images/projects/Warehouse.jpg";
+import imperial from "../../public/images/projects/imperial.jpg";
+import Project1 from "../../public/images/projects/dummy.png";
 
 const FramerImage=motion(Image);
 
@@ -79,20 +82,20 @@ const projects = () => {
             </div>
             <div className="col-span-4">
                 <Project
-                title="Live Crypto Tracker"
-                img={Project1}
-                link="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
-                type="Project"
-                github="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
+                title="Construction Management App"
+                img={constructionApp}
+                link="https://github.com/Madura-Prasad/Construction-App.git"
+                type="App Development Project (Campus)"
+                github="https://github.com/Madura-Prasad/Construction-App.git"
                 />
             </div>
             <div className="col-span-4">
             <Project
-                title="Live Crypto Tracker"
-                img={Project1}
-                link="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
-                type="Project"
-                github="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
+                title="Warehouse Management System"
+                img={Warehouse}
+                link="https://github.com/Madura-Prasad/Warehouse-Desktop-Application.git"
+                type="JAVA OOP Project (Campus)"
+                github="https://github.com/Madura-Prasad/Warehouse-Desktop-Application.git"
                 />
             </div>
             <div className="col-span-4">
@@ -106,11 +109,11 @@ const projects = () => {
             </div>
             <div className="col-span-12">
             <FeaturedProject
-                title="Live Crypto Tracker"
-                img={Project1}
-                summary="The live crypto tracker is a mobile app developed using React Native and CoinGecko API that provides real-time cryptocurrency price monitoring. It features a user-friendly interface, customizable watchlists, price alerts, push notifications, and detailed views of each cryptocurrency. The app aims to provide a reliable and convenient tool for crypto enthusiasts to stay up-to-date and make informed investment decisions."
+                title="Imperial Educational Center"
+                img={imperial}
+                summary="The Imperial College HD Final Project is a web application developed using PHP, JavaScript, jQuery, and MySQL. The goal of the project is to create a platform that allows students to access various resources related to their studies and interact with each other. The platform features a dashboard where students can view their course schedules, grades, and announcements from their professors. Additionally, students can join discussion forums and chat rooms to collaborate with their peers. The platform also includes a search function that allows students to find relevant resources, such as study guides and past exam papers. The system is designed to be user-friendly and intuitive, with a modern and visually appealing interface. Overall, the Imperial College HD Final Project aims to enhance the learning experience for students and facilitate communication and collaboration within the academic community."
                 link="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
-                type="Featured Project"
+                type="Higher Diploma Final Project (Campus)"
                 github="https://github.com/Madura-Prasad/Live-Crypto-Tracker.git"
 
             />
