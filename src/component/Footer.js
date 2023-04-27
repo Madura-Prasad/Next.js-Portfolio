6efd8707843cdc2d0820b69448abbf452248fab9
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-28 01:15:04 +0530

    home page responsive

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light'>
-      <Layout className='py-5 flex items-center justify-between cursor-pointer'>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base'>
+      <Layout className='py-5 flex items-center justify-between cursor-pointer lg:flex-col lg:py-6'>
         <span>  <Link href="https://www.linkedin.com/in/madura-prasad/" className='underline underline-offset-2' target={"_blank"}> Madura Prasad</Link> &copy; {new Date().getFullYear()}</span>
       
       </Layout>
