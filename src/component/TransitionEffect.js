88971e29132d19cdbba5ddd79a1b1d1e5aa84207
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-29 19:46:34 +0530

    change color transition

--- src/component/TransitionEffect.js
+++ src/component/TransitionEffect.js
@@ -5,20 +5,20 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30  bg-light dark:bg-dark"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         exit={{ x: ["0%", "100%"] , width:["0%", "100%"]}}
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-primary"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-primaryDark"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
