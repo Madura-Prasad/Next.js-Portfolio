b9697b0f2912b3ddac29f03087dce6711d0d870f
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-05-08 01:16:45 +0530

    update resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
                   href="/Madura Prasad-Resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-bold border-transparent hover:border-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
-                  download={true}
+                  
                 >
                   Resume
                   <LinkArrow className={"w-6 ml-1"} />
